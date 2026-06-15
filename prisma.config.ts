f0c287f22022c3270d6fc82ea58143a5a345098b
Frida <Frida.bredberg@extrapreneur.se>
2026-06-15 06:51:11 +0200

    Fix type error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,10 @@
-import "dotenv/config";
 import path from "node:path";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
   migrations: {
     path: "prisma/migrations",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
   },
 });
 
